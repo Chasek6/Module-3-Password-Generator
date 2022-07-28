7cf3f2cac897982bd23ea0fedd8887dcf10afb73
Chase Stratton <chasestratton@Chases-MacBook-Pro.local>
2022-07-28 18:36:48 -0400

    editting concat

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ var numCharacters = ['0', '2', '3', '4', '5', '6', '7','8','9'];
 var upperCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var specCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var UserPick = [];
 // var passwordLength = "";
-// var UserPick = [];
 
 // input values and validate //
 // prompt//
@@ -34,7 +34,7 @@ if (!passwordLength) {
     var confirmSpec = confirm("Would you like special characters in your password?");
 
     if (confirmLower) {
-        UserPick = confirmLower.concat(UserPick);
+      var  UserPick = confirmLower.concat(UserPick);
     }
     if (confirmNum) {
         UserPick = confirmNum.concat(UserPick);
@@ -68,7 +68,7 @@ var generateBtn = document.querySelector("#generate")
 function writePassword() {
 var password = generatePassword();
 var passwordText = document.querySelector("password");
-passwordText.value = password;
+passwordText.value = passwordResult;
 
 }
 
